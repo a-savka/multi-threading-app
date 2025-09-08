ce0fe81c23b768b536bc1e76922fe267ad4fe36e
Andrey Savka <a_sawka@yahoo.com>
2025-09-08 22:30:39 +0300

    refactor-workers-list

--- src/main/java/ru/savka/demo/service/WorkerService.java
+++ src/main/java/ru/savka/demo/service/WorkerService.java
@@ -2,29 +2,39 @@ package ru.savka.demo.service;
 
 import org.springframework.stereotype.Service;
 import ru.savka.demo.dto.WorkerInfo;
+import ru.savka.demo.model.Worker;
 
 import jakarta.annotation.PostConstruct;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class WorkerService {
 
-    private final List<WorkerInfo> workers = new ArrayList<>();
+    private final List<Worker> workers = new ArrayList<>();
 
     @PostConstruct
     public void init() {
         FetcherThread fetcherThread = new FetcherThread("currency-data-fetcher");
-        workers.add(new WorkerInfo(fetcherThread.getName(), LocalDateTime.now()));
+        workers.add(new Worker(
+                new WorkerInfo(fetcherThread.getName(), LocalDateTime.now()),
+                fetcherThread
+        ));
         fetcherThread.start();
 
         Thread parserThread = new Thread(new ParserThread(), "currency-data-parser");
-        workers.add(new WorkerInfo(parserThread.getName(), LocalDateTime.now()));
+        workers.add(new Worker(
+                new WorkerInfo(parserThread.getName(), LocalDateTime.now()),
+                parserThread
+        ));
         parserThread.start();
     }
 
     public List<WorkerInfo> getWorkers() {
-        return workers;
+        return workers.stream()
+                .map(Worker::getIno)
+                .collect(Collectors.toList());
     }
 }
