f23227f3bc60d9459ffa963199bf266a1341494b
Andrey Savka <a_sawka@yahoo.com>
2025-12-27 00:09:54 +0300

    reports and presentation

--- src/main/java/ru/savka/demo/config/OpenTelemetryConfig.java
+++ src/main/java/ru/savka/demo/config/OpenTelemetryConfig.java
@@ -1,15 +1,26 @@
 package ru.savka.demo.config;
 
 import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
+import io.opentelemetry.sdk.resources.Resource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenTelemetryConfig {
 
+    private static final AttributeKey<String> SERVICE_NAME_KEY = AttributeKey.stringKey("service.name");
+
     @Bean
     public OpenTelemetry openTelemetry() {
-        return AutoConfiguredOpenTelemetrySdk.builder().build().getOpenTelemetrySdk();
+        return AutoConfiguredOpenTelemetrySdk.builder()
+                .addResourceCustomizer((resource, configProperties) ->
+                        resource.toBuilder()
+                                .put(SERVICE_NAME_KEY, "currency-rate-app")
+                                .build())
+                .build()
+                .getOpenTelemetrySdk();
     }
 }
